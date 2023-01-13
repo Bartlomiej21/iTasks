b6a3ceb2c0054c4f2c4b2b5577071266cc66b1f1
Marcin Hajduczek <marcin.hajduczek@garagenet.com>
2023-01-13 10:20:24 +0100

    Add 2nd task

---
+++ src/main/java/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+
+    /*
+    TODO: Write a Java program that will result in deadlock, then change it to remove deadlock.
+     */
+    public static void main(String[] args) {
+
+    }
+}
