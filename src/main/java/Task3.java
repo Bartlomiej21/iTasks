91d437fbe3be387f16c79364a02188b5dc5e6fcc
Marcin Hajduczek <marcin.hajduczek@garagenet.com>
2023-11-14 12:45:44 +0100

    wip

---
+++ src/main/java/Task3.java
@@ -0,0 +1,23 @@
+public class Task2 {
+    /*
+    TODO: write a program to check if given year is a leap year
+     */
+    public static void main(String[] args) {
+        solution1(1920);
+        solution2(1920);
+        solution1(1996);
+        solution2(1996);
+        solution1(2000);
+        solution2(2000);
+        solution1(2100);
+        solution2(2100);
+    }
+
+    public static void solution1(int y) {
+
+    }
+
+    public static void solution2(int y) {
+
+    }
+}
